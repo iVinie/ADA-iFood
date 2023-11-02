67744e75775946733ddaed99307dc745f87ead78
Vinicius Oliveira <vinicius.dsro@hotmail.com>
2023-11-01 21:40:38 -0300

    Atividade da aula 11, dia: 01/11/23

--- 02-Logica-prog-2-py/Atividades/02-Aula-ADA-iFood.py
+++ 02-Logica-prog-2-py/Atividades/02-Aula-ADA-iFood.py
@@ -9,20 +9,36 @@ Saída:
 ["chave2", "chave3"]
 Em complemento, peça que o usuário informe cada chave e valor que será armazenado no dicionário, e também, qual valor ele deseja buscar pela função. O programa deverá permitir que o usuário digite 10 chaves e 10 valores e 1 valor a ser buscado.
 '''
-def dicionario_valor(dicionario:{}, valor):
+def dicionario_valor(dicionario, valor):
     lista = []
     for chave, valor_dicionario in dicionario.items():
         if valor == valor_dicionario:
             lista.append(chave)
     return lista
-dicionario = { "chave1": 4, "chave2": 5, "chave3": 5, "chave4": 1 }
-valor = 5
+
+qtd = int(input('Quantos valores quer digitar: '))
+while qtd <= 0 or qtd > 10:
+    qtd = int(input('Quantidade invalida (entre 1 e 10) quantos valores quer digitar: '))
+    
+dicionario = {}
+for i in range (0, qtd):
+    chave = input(f'Qual a {i + 1} chave: ')
+    valor = int(input(f'Qual o {i + 1} valor: '))
+    while chave in dicionario:
+        print('Chave repetida!')
+        chave = input(f'Qual a {i + 1} chave: ')
+    dicionario.update({chave: valor})
+
+valor = int(input('Qual valor deseja buscar: '))
 lista = dicionario_valor(dicionario, valor)
-print(lista)
+if len(lista) == 0:
+    print ("Não tem chave com esse valor")
+else:
+    print(lista)
 
 
 '''
-Faça uma função chamada conta_letras que receba uma frase digitada pelo usuário, e que seja capaz de contar a ocorrência de cada palavra na frase recebida. Para implementar esta função, utilize dicionários.
+Faça uma função chamada conta_letras que receba uma frase digitada pelo usuário, e que seja capaz de contar a ocorrência de cada letra na frase recebida. Para implementar esta função, utilize dicionários.
 Exemplo:
 
 Entrada:
@@ -45,7 +61,7 @@ def contar_letras(frase):
             unicas[letra] += 1
     return unicas
 
-frase = "Dicionários são legais"
+frase = input('Qual a frase: ')
 print(frase)
 resultado = contar_letras(frase)
 for letra, qtd in resultado.items():
@@ -67,7 +83,7 @@ def contar_palavras(frase):
             unicas[palavra] += 1
     return unicas
 
-frase = "o mar é Azul pq o ceu é azul"
+frase = input('Qual a frase: ')
 print(frase)
 resultado_2 = contar_palavras(frase)
 for palavra, qtd in resultado_2.items():
@@ -84,7 +100,13 @@ def dicionario_notas(lista_numero, lista_string):
     for chave, valor in zip(lista_string, lista_numero):
         dicionario.update({chave: valor})
     return dicionario
-lista_numeros = [10, 8, 5, 7]
+lista_numeros = []
+for i in range(0, 4):
+    nota = input(f'Qual a nota {i+1}: ')
+    while not nota.isdigit():
+        nota = input(f'Nota invalida, qual a nota {i+1}: ')
+    nota = int(nota)
+    lista_numeros.append(nota)
 lista_strings = ["nota1", "nota2", "nota3", "nota4"]
 dicionario = dicionario_notas(lista_numeros, lista_strings)
 print (dicionario)
